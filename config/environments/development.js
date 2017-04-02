c1dff69afba043751729386289d415222e15de6e
Pasca Ionut <pascaionut94@yahoo.com>
2017-04-02 20:44:36 +0300

    Added skelet

---
+++ config/environments/development.js
@@ -0,0 +1,7 @@
+module.exports = {
+    port: 8080,
+    host: '127.0.0.1',
+    app: {
+        name: 'Macaroon API'
+    }
+};
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const config = require('./config/main');
+const ENV = process.env.NODE_ENV || 'development';
+
+let server = null;
+const quark = require('quark')();
+
+let app = express();
+app.set('config', config);
+app.set('root', __dirname);
+app.set('env', ENV);
+
+app.use((err, req, res, next) => {
+    res.status(500).json(err);
+});
+
+if (!module.parent) {
+    server = http.createServer(app);
+    server.listen(config.port || 3000);
+    console.log(`${config.app.name} is running, listening on port ${config.port}, environment: ${ENV.toLowerCase()}`);
+};
